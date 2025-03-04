b83b2ae6503daed6339b2d74b162040a00904085
DaggerFn <gustavodagger02@outlook.com>
2025-03-04 16:41:55 -0300

    flask-cors add

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,12 @@
+from flask_cors import CORS
 from flask import Flask
 
 def create_app():
     app = Flask(__name__)
+    
+    # Habilitar CORS para todos os métodos
+    CORS(app, resources={r"/*": {"origins": "*", "methods": ["GET", "POST", "PUT", "DELETE"]}})
+    
     app.config["DATABASE"] = "instance/meu_banco.sqlite"
 
     from app.routes import init_routes
--- app/routes.py
+++ app/routes.py
@@ -11,14 +11,19 @@ def init_routes(app):
     @app.route("/materiais", methods=["POST"])
     def create_material():
         data = request.get_json()
+
+        if not data:
+            return jsonify({"error": "Nenhum dado enviado"}), 400
+
         conn = get_db()
         cursor = conn.execute(
             "INSERT INTO materiais (id_material, locale_material, description_material) VALUES (?, ?, ?)",
             (data["id_material"], data["locale_material"], data["description_material"])
         )
         conn.commit()
-        return jsonify({"id_material": data["id_material"], "message": "Material inserido com sucesso!"}), 201
-        # Rota para deletar um material pelo ID
+
+        return jsonify({"message": "Material inserido com sucesso!"}), 201
+
     
     @app.route("/materiais/<id_material>", methods=["DELETE"])
     def delete_material(id_material):
